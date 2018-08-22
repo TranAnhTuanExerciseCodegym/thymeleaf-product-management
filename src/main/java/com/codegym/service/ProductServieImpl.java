0b8c020049a4da908caf08157b14871bd90dee1b
trantuan <tincanxh@gmail.com>
2018-08-22 13:44:26 +0700

    add model and service

---
+++ src/main/java/com/codegym/service/ProductServieImpl.java
@@ -0,0 +1,41 @@
+package com.codegym.service;
+
+import com.codegym.model.Product;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ProductServieImpl implements ProductService {
+    private static Map<Integer, Product> productMap = new HashMap<>();
+    static {
+        productMap.put(1, new Product(1, "Samsung", "J7 Pro", 50000));
+    }
+
+
+    @Override
+    public List<Product> findAll() {
+        return new ArrayList<>(productMap.values());
+    }
+
+    @Override
+    public void save(Product product) {
+        productMap.put(product.getId(), product);
+    }
+
+    @Override
+    public Product findById(int id) {
+        return productMap.get(id);
+    }
+
+    @Override
+    public void update(int id, Product product) {
+        productMap.put(id, product);
+    }
+
+    @Override
+    public void remove(int id) {
+        productMap.remove(id);
+    }
+}
